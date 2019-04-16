95a18d361c2e6be9f057fc9f1e264f3c23d54c73
Nikhil <nikhilprasadthakur@gmail.com>
2019-04-16 23:51:51 +0530

    acc details service added

---
+++ accountdetails-service/accountdetails-service/src/main/java/com/practice/mypay/accountdetailsservice/model/Customer.java
@@ -0,0 +1,39 @@
+package com.practice.mypay.accountdetailsservice.model;
+
+public class Customer {
+
+
+    private String phoneNumber;
+
+    private String name;
+
+
+    private Wallet wallet;
+
+    public Customer() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+}
